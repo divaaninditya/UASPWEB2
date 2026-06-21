cd88394fa2f7f134dbd4e605365021c4529f72eb
divaaninditya <dianindi77@gmail.com>
2026-06-21 21:58:31 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+
+$conn = mysqli_connect(
+"uaspweb2.infinityfreeapp.com"
+"if0_42236249"
+"e2Vdhtke8zrD"
+"if0_42236249_web2_uas"
+);
+
+if(!$conn){
+die("Database gagal terkoneksi");
+}
+
+?>
